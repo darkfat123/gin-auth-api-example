c86f07e62852e4c270684eb7dae86107a92e898a
darkfat123 <supakorn.yookack@gmail.com>
2025-04-22 20:46:51 +0700

    feat: improve CORS middleware to restrict allowed origins and handle OPTIONS requests

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,13 +1,26 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
 
-// ปรับให้ไม่มีการตอบกลับ OPTIONS
+	"github.com/gin-gonic/gin"
+)
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // หรือระบุ domain เช่น https://example.com
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		origin := c.GetHeader("Origin")
+		if origin == os.Getenv("ALLOWED_ORIGINS") {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		}
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		c.Next()
 	}
 }
